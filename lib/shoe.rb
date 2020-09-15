460d12d686c4b7250f3f1454da87fd239185358e
Leah Schlackman <leahemily1@gmail.com>
2020-09-15 20:54:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     @@BRANDS << brand
-    @@BRANDS = @@BRANDS.uniq 
+    @@BRANDS = @@BRANDS.uniq {|brand|}
     @@BRANDS
     # @@BRANDS = @@BRANDS.uniq
   end
