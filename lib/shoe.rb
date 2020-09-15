c1440ec02f9241f97291de27c52e1470f31ca439
Leah Schlackman <leahemily1@gmail.com>
2020-09-15 21:15:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,10 @@ class Shoe
  
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(@brand))
-      BRANDS << @brand
-      BRANDS
-    # @@BRANDS = @@BRANDS.uniq
+    # if !(BRANDS.include?(@brand))
+    #   BRANDS << @brand
+    #   BRANDS
+    # # @@BRANDS = @@BRANDS.uniq
     end
   end 
 
